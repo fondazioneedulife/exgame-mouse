81e062425379c8cc57ee9cc69176240e6bec2e72
aronneavesani@gmail.com <aronneavesani@gmail.com>
2025-09-23 12:46:48 +0200

    lezione2

---
+++ frontend/src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <div>
+          <img src="\src\\assets\\ExGame logo.png" alt="" />
+          <li>
+            <ul>Dashboard</ul>
+            <ul>Esami</ul>
+          </li>
+        </div>
+        <div>
+          <li>
+            <ul>Ale Falezza</ul>
+            <ul>Logout</ul>
+          </li>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default App
