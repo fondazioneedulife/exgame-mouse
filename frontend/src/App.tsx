3fdc51bd3a6fcb12f3146158507813914ee19dff
Ricky0027 <borsattoriccardo02@gmail.com>
2025-09-23 12:40:19 +0200

    commit seconda lezione

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <body>
+      <nav className="navbar">
+        <div className="logo">
+          <img src="\src\\assets\\logo.png" alt="Exgame" />
+        </div>
+        <div className="header">
+          <ul className="navbar-links">
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Esami</a></li>
+            <li className='login'><a href="">Login</a></li>
+            <li><a href="">Registrati</a></li>
+          </ul>
+        </div>
+
+      </nav>
+    </body>
+  )
+}
+
+export default App
