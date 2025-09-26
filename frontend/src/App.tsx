b9150bf7c0c927d7c2b69bd8702cd679fe3b8583
GiuliaSole <aldegherigiuliasole@gmail.com>
2025-09-26 12:36:08 +0200

    b

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import styles from "./App.module.css";
 import Logo from "./assets/ExGame logo.svg";
 import Icona from "./assets/profilo.png";
+import Test from './Test.tsx'
 
 function App() {
   return (
@@ -28,7 +29,7 @@ function App() {
           <ul>
             <li>
               <a href="#" className={styles.dx}>
-                Albe Molon
+                Giulia Sole
               </a>
             </li>
             <li>
@@ -47,8 +48,52 @@ function App() {
             <p>Alberto Molon</p>
           </div>
         </div>
+        <div className={styles.contentInterno}>
+          <h1>Test di Matematica - Classe 1A</h1>
+        </div>
+        <div className= {styles.informazioni}>
+          <p className = {styles.infSingola}>date: 26 settembre 2025</p>
+          <p className = {styles.infSingola}>session: prima sessione</p>
+          <p className = {styles.infSingola}>teacher: Prof. Bianche</p>
+        </div>
+        <div className = {styles.timer}>
+          <p className={styles.scritta}>Tempo rimasto:</p>
+          <p className={styles.scrittaDue}>00:25:00</p>
+        </div>
+        <div className= {styles.gruppiDomande}>
+          <div className= {styles.domanda}>
+            <h3>Quanto fa 2 + 2</h3>
+            <label><input type="radio" name="2piu2"/> 3</label>
+            <label><input type="radio" name="2piu2"/> 4</label>
+            <label><input type="radio" name="2piu2"/> 5</label>
+            <label><input type="radio" name="2piu2"/> Pesce</label>
+          </div>
+          <div className= {styles.domanda}>
+            <h3>Quanto fa 3 x 3</h3>
+            <label><input type="radio" name="3per3"/> 6</label>
+            <label><input type="radio" name="3per3"/> 9</label>
+            <label><input type="radio" name="3per3"/> 12</label>
+            <label><input type="radio" name="3per3"/> Per3, quindi pere</label>
+          </div>
+          <div className= {styles.domanda}>
+            <h3>Quante caramelle rimangono se ne ha 10 e ne mangi 2?</h3>
+            <label><input type="radio" name="caramelle"/> 2</label>
+            <label><input type="radio" name="caramelle"/> 4</label>
+            <label><input type="radio" name="caramelle"/> 8</label>
+            <label><input type="radio" name="caramelle"/> 0, le altre le ho mangiate io :P</label>
+          </div>
+          <div className= {styles.domanda}>
+            <h3>Quante gambe hanno due giraffe e mezzo?</h3>
+            <label><input type="radio" name="serpenti"/> 6</label>
+            <label><input type="radio" name="serpenti"/> 8</label>
+            <label><input type="radio" name="serpenti"/> 10</label>
+            <label><input type="radio" name="serpenti"/> 0, in realtà erano serpenti</label>
+          </div>
+        </div>
       </div>
 
+      
+      
       {/* <Description type="info"> Oggi facciamo:
         <ul>
           <li>Esercizio 1</li>
---
+++ frontend/src/Test.tsx
@@ -0,0 +1,11 @@
+import './Test.css'
+
+function Test() {
+
+  return (
+    <>
+    <p> ciao</p>
+    </>
+)
+}
+export default Test
