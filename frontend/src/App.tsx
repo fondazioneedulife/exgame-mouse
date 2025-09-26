6d95c398a159c75bb16c14f4504b6f8c29a80729
elisapozzatti <elisa.pozzatti@gmail.com>
2025-09-26 11:53:22 +0200

    continuato grafica pagina test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
 import styles from "./App.module.css";
 import Logo from "./assets/ExGame logo.svg";
 import Icona from "./assets/profilo.png";
+import { useState } from "react";
 
 function App() {
+  const [scelta, setScelta] = useState("");
+  const today = new Date().toLocaleDateString("it-IT", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });;
   return (
     <>
       <nav className={styles.navbar}>
@@ -28,7 +35,7 @@ function App() {
           <ul>
             <li>
               <a href="#" className={styles.dx}>
-                Albe Molon
+                Elisa Pozzatti
               </a>
             </li>
             <li>
@@ -44,12 +51,101 @@ function App() {
         <div className={styles.content}>
           <div className={styles.userInfo}>
             <img src={Icona} alt="" className={styles.icona} />
-            <p>Alberto Molon</p>
+            <p>Elisa Pozzatti</p>
           </div>
         </div>
       </div>
 
-      {/* <Description type="info"> Oggi facciamo:
+      <div className={styles.main}>
+        <div className={styles.content}>
+          <div className={styles.testTitle}>
+            <p>Test di Matematica - Classe 1A</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.mainInfos}>
+        <div className={styles.contentInfos}>
+          <div className={styles.date}>
+            <p>date: {today}</p>
+          </div>
+          <div className={styles.session}>
+            <p>session: Prima sessione</p>
+          </div>
+          <div className={styles.teacher}>
+            <p>teacher: Prof. Bianchi</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.main}>
+        <div className={styles.contentTime}>
+          <div className={styles.timeRemain}>
+            <p>Tempo rimasto:</p>
+          </div>
+          <div className={styles.time}>
+            <p>00:25:00</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.main}>
+        <div className={styles.content}>
+          <div className={styles.question}>
+            <p>Quanto fa 2+2?</p>
+            <label>
+              <input
+                type="radio"
+                name="scelta"
+                value="a"
+                checked={scelta === "a"}
+                onChange={(e) => setScelta(e.target.value)}
+              />
+              3
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="scelta"
+                value="b"
+                checked={scelta === "b"}
+                onChange={(e) => setScelta(e.target.value)}
+              />
+              4
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="scelta"
+                value="c"
+                checked={scelta === "c"}
+                onChange={(e) => setScelta(e.target.value)}
+              />
+              5
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="scelta"
+                value="d"
+                checked={scelta === "d"}
+                onChange={(e) => setScelta(e.target.value)}
+              />
+              Dipende dalla fantasia
+            </label>
+        </div>
+      </div>
+    </div >
+
+    <div className={styles.main}>
+        <div className={styles.contentEndTest}>
+          <div className={styles.endTest}>
+            <p>Hai terminato? Consegna!</p>
+          </div>
+        </div>
+      </div>
+
+    {/* <Description type="info"> Oggi facciamo:
         <ul>
           <li>Esercizio 1</li>
           <li>Esercizio 2</li>
