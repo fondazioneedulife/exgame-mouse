282427fc66901c69e4e6b535fdeacb849084a427
popipo-dot <76meonie@gmail.com>
2025-09-26 12:57:12 +0200

    Merge pull request #3 from fondazioneedulife/layout-react
    commit terza lezione

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -41,11 +41,52 @@ function App() {
       </nav>
 
       <div className={styles.main}>
+
         <div className={styles.content}>
+
+          {/* INFO STUDENTE */}
           <div className={styles.userInfo}>
             <img src={Icona} alt="" className={styles.icona} />
             <p>Alberto Molon</p>
           </div>
+
+          {/* TITOLO ESAME */}
+          <h1 className={styles.examTitle}>Test di Matematica - Classe 1A</h1>
+
+          {/* INFO ESAMI BOX */}
+          <div className={styles.examInfoBox}>
+            <p className={styles.examInfo}>
+              date: 12 settembre 2025
+            </p>
+            <p className={styles.examInfo}>
+              session: Prima sessione
+            </p>
+            <p className={styles.examInfo}>
+              teacher: Prof. Bianchi
+            </p>
+          </div>
+
+          {/* BOX TIMER */}
+          <div className={styles.timerBox}>
+            <h3>Tempo rimasto:</h3>
+            <p>00:25:00</p>
+          </div>
+          
+          {/* DOMANDE */}
+
+            <h3>Quanto fa 2 + 2?</h3>
+            
+            <input type="radio" id="four" name="fav_language" value="4" />
+            <label htmlFor="four">4</label><br/>
+            <input type="radio" id="five" name="fav_language" value="5" />
+            <label htmlFor="five">5</label><br/>
+            <input type="radio" id="dipende" name="fav_language" value="Dipende dalla fantasia" />
+            <label htmlFor="dipende">Dipende dalla fantasia</label><br/>
+
+            <hr />
+
+          
+
         </div>
       </div>
 
