dbf8a8d8046516d9d914f11b9aee281ff6af30a1
Molon <ale419129@gmail.com>
2025-09-30 09:57:01 +0200

    spostamento dei dati nei vari file

--- frontend/src/pages/Subscription.tsx
+++ frontend/src/pages/Subscription.tsx
@@ -1,3 +1,23 @@
-/**
- * Il componente Subscription rappresenta la pagina che visualizza uno studente quando esegue un esame.
- */
+import ClockComponent from "../components/Clock/ClockComponent";
+import Description from "../components/Description/Description";
+import QuestionList from "../components/QuestionList/QuestionList";
+import UserInfoComponent from "../components/UserInfo/UserInfoComponent";
+import ChipList from "../components/ChipList/ChipList";
+
+import { chips } from "../mocks/chips";
+import { questions } from "../mocks/questions";
+
+
+function Subscription () {
+return(
+    <>
+        <UserInfoComponent testo="Ceci Florea"></UserInfoComponent>
+          <Description classe="1A" tipoDiTest="Matematica"></Description>
+          <ChipList chips={chips}></ChipList>
+          <ClockComponent tempo={7200}></ClockComponent>
+          <QuestionList QuestionsList={questions} />
+    </>
+)
+} 
+
+export default Subscription
