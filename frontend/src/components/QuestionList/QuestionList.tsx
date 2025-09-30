df0ecefbc1c647f3415f81a1c078ba9c5d130335
Jacopo Sassi <sassijacopp@gmail.com>
2025-09-30 12:20:42 +0200

    Fix type import for QuestionComponentProps and improve responses state initialization

--- frontend/src/components/QuestionList/QuestionList.tsx
+++ frontend/src/components/QuestionList/QuestionList.tsx
@@ -1,23 +1,25 @@
 
-import type { QuestionComponentProp } from "./QuestionComponent/QuestionComponent";
+import type { QuestionComponentProps } from "./QuestionComponent/QuestionComponent";
 import QuestionComponent from "./QuestionComponent/QuestionComponent";
 import { useState } from "react";
 
 
 type QuestionList = {
-  QuestionsList: QuestionComponentProp[];
+  QuestionsList: QuestionComponentProps[];
 };
 
 const QuestionList = ({ QuestionsList }: QuestionList) => {
-  const [responses, setResponses] = useState({
-    "0":"Marsiglia",
-    "1":"Petrarca",
+  const [responses, setResponses] = useState<{ [key: number]: string }>({
+    0: "Marsiglia",
+    1: "Petrarca",
   });
+
+  console.log ("Lo stato in questo momento é: ", JSON.stringify(responses, null, 2));
 
   return (
     <div className="QuestionList">
       {QuestionsList.map((Question, idx) => (
-        <QuestionComponent key={idx} question={Question.question} answers={Question.answers} response ={responses[idx]} setResponse = {(value: string) => {setResponses ({...responses,[idx]: value})} }/>
+        <QuestionComponent key={idx} question={Question.question} answers={Question.answers} response={responses[idx]} setResponse={(value: string) => { setResponses({ ...responses, [idx]: value }) }} />
       ))}
     </div>
   );
