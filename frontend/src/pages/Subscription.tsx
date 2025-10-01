b8fa6f61f43736a49247a68d5bb385b9939d1de5
Jacopo Sassi <sassijacopp@gmail.com>
2025-10-01 11:00:53 +0200

    Wrote the mock for API and commented the structure of questions for changing the data format

--- frontend/src/pages/Subscription.tsx
+++ frontend/src/pages/Subscription.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import ChipList from "../components/ChipList/ChipList";
 import ClockComponent from "../components/Clock/ClockComponent";
 import Description from "../components/Description/Description";
@@ -6,14 +7,30 @@ import UserInfoComponent from "../components/UserInfo/UserInfoComponent";
 import { chips } from "../mocks/chips";
 import { questions } from "../mocks/questions";
 
+const useAPIData = (url: string, defaultState: unknown) => {
+  const [state, setState] = useState(defaultState);
+
+  useEffect(() => {
+    // fetch(url).then(
+    //   response
+    //     .then((response) => response.json())
+    //     .then((response) => setState(response)),
+  //   );
+  setState(questions);
+  }, []);
+
+  return state;
+};
+
 export const Subscription = () => {
+  const exam = useAPIData("http://localhost:8080/exams/matematica", []);
   return (
     <>
       <UserInfoComponent testo="Alberto Molon"></UserInfoComponent>
       <Description classe="1A" tipoDiTest="Matematica"></Description>
       <ChipList chips={chips}></ChipList>
       <ClockComponent tempo={7200}></ClockComponent>
-      <QuestionList questionsList={questions} />
+      <QuestionList questionsList={exam.questions} />
     </>
   );
 };
