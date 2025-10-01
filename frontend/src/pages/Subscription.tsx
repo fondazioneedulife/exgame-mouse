04532924aeffd41fbeb335ab2627462a45e7ccf8
siotta <alessialongo2005@gmail.com>
2025-10-01 09:18:32 +0200

    lezione-4

--- frontend/src/pages/Subscription.tsx
+++ frontend/src/pages/Subscription.tsx
@@ -1,3 +1,22 @@
-/**
- * Il componente Subscription rappresenta la pagina che visualizza uno studente quando esegue un esame.
- */
+
+import UserInfoComponent from "../components/UserInfo/UserInfoComponent";
+import Description from "../components/Description/Description";
+import ChipList from "../components/ChipList/ChipList";
+import ClockComponent from "../components/Clock/ClockComponent";
+import QuestionList from "../components/QuestionList/QuestionList";
+import { chips } from "../mocks/chips";
+import { questions } from "../mocks/questions";
+
+function Subscription() {
+  return (
+    <>
+      <UserInfoComponent testo="Alberto Molon" />
+      <Description classe="1A" tipoDiTest="Matematica" />
+      <ChipList chips={chips} />
+      <ClockComponent tempo={7200} />
+      <QuestionList QuestionsList={questions} />
+    </>
+  );
+}
+
+export default Subscription;
