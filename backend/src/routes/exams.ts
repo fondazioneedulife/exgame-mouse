9367a50f32127db7e16a6159fcc3ffaaa3348b98
Jacopo Sassi <sassijacopp@gmail.com>
2025-10-02 12:19:00 +0200

    feat(exams): define Exam type and initialize exams array with sample data

--- backend/src/routes/exams.ts
+++ backend/src/routes/exams.ts
@@ -1,10 +1,27 @@
 import Router from "@koa/router";
 import ExamsMocks from "../mocks/examsMocks";
 
-let exams =[]
+type Exam = {
+  id: number;
+  name: string;
+  date?: string;
+  description?: string;
+  schedule_date?: string;
+  max_time?: number;
+};
+
+let exams: Exam[] = [
+  {
+    id: 1,
+    name: "Matematica",
+    schedule_date: "2025-11-15",
+    max_time: 1200,
+  },
+  { id: 2, name: "Fisica", schedule_date: "2025-11-15", max_time: 1200 },
+];
 
 const router = new Router({
-  prefix: "/api"
+  prefix: "/api",
 });
 
 router.get("/exams", async (ctx) => {
@@ -13,24 +30,27 @@ router.get("/exams", async (ctx) => {
 });
 
 router.get(`/exams/:id`, async (ctx) => {
-  ctx.body = ExamsMocks._id === ctx.params.id ? ExamsMocks : { message: "Exam not found" };
+  ctx.body =
+    ExamsMocks._id === ctx.params.id
+      ? ExamsMocks
+      : { message: "Exam not found" };
   ctx.status = 200;
 });
 
 router.post("/exams", async (ctx) => {
   try {
-   const {_id, name, schedule_date, max_time} = ctx.request.body;
+    const { _id, name, schedule_date, max_time } = ctx.request.body;
 
-    if(!_id || !name || !schedule_date || !max_time){
+    if (!_id || !name || !schedule_date || !max_time) {
       ctx.body = { message: "Missing fields" };
       ctx.status = 400;
       return;
     }
-     const newExam = {
+    const newExam = {
       id: exams.length + 1,
       name,
       schedule_date,
-      max_time
+      max_time,
     };
 
     exams.push(newExam);
@@ -46,7 +66,38 @@ router.post("/exams", async (ctx) => {
 });
 
 router.patch("/exams", async (ctx) => {
-  
+  try {
+    const { id, name, schedule_date, max_time } = ctx.request.body;
+
+    if (!id || !name || !schedule_date || !max_time) {
+      ctx.body = { message: "Missing fields" };
+      ctx.status = 400;
+      return;
+    }
+
+    const index = exams.findIndex((exam) => exam.id === id);
+
+    if (index === -1) {
+      ctx.body = { message: "Exam not found" };
+      ctx.status = 404;
+      return;
+    }
+
+    exams[index] = {
+      id,
+      name,
+      schedule_date,
+      max_time,
+    };
+
+    ctx.status = 200;
+    ctx.body = exams[index];
+  } catch (err) {
+    console.error("Errore durante la modifica dell'esame:", err);
+    ctx.status = 500;
+    ctx.body = { error: "Errore interno del server" };
+  }
+   console.log(exams);
 });
 
 router.delete("/exams", async (ctx) => {
