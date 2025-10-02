85f55ac946b60e845773b9c5ea577a472f8fd4e0
elisapozzatti <elisa.pozzatti@gmail.com>
2025-10-02 12:45:03 +0200

    completate chiamate, manca robustezza

--- backend/routes/exams.ts
+++ backend/routes/exams.ts
@@ -4,12 +4,34 @@ import { exams } from "../src/mocks/exams"
 const examsRouter = new Router();
 
 examsRouter.get("/api/exams", async (ctx) => {
-    ctx.body = exams
+    ctx.body = exams;
     ctx.status = 200;
 })
 
 examsRouter.get("/api/exams/:id", async (ctx) => {
-    ctx.body = exams.find(exams => ctx.params.id === exams._id)
+    ctx.body = exams.find(exams => ctx.params.id === exams._id);
+    ctx.status = 200;
+})
+
+examsRouter.post("/api/exams", async (ctx) => {
+    const newExam = ctx.request.body;
+    exams.push(newExam);
+    ctx.body = exams;
+    ctx.status = 200;
+})
+
+examsRouter.patch("/api/exams/:id", async (ctx) => {
+    const findId = exams.findIndex(exams => ctx.params.id === exams._id);
+    const updates = ctx.request.body;
+    exams[findId] = {...updates};
+    ctx.body = `Aggiornato l'esame ${findId}`;
+    ctx.status = 200;
+})
+
+examsRouter.delete("/api/exams/:id", async (ctx) => {
+    const findId = exams.findIndex(exams => ctx.params.id === exams._id);
+    const removedExam = exams.splice(findId, 1);
+    ctx.body = `Rimosso l'esame ${findId}`
     ctx.status = 200;
 })
 
