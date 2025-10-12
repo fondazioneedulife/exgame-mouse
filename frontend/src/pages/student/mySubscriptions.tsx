b07e3d7c59fc7903cce4a2d7acb5e0da9b2650d5
Alessandro Falezza <alessandro.falezza@intesyt.it>
2025-10-12 12:20:41 +0200

    feat(mySubscriptions): fetch and display user subscriptions

--- frontend/src/pages/student/mySubscriptions.tsx
+++ frontend/src/pages/student/mySubscriptions.tsx
@@ -1,14 +1,76 @@
+import { useEffect, useState } from "react";
+import type { ExamType, SubscriptionType } from "../../../../api/types";
 import { Table } from "../../components/Table/Table";
-import { mySubscriptions } from "../../mocks/mySubscriptions";
+
+export type SubscribedExam = ExamType &
+  Pick<SubscriptionType, "status" | "grade">;
+
+const useMySubscribedExams = () => {
+  const [mySubscribedExams, setMySubscribedExams] = useState<SubscribedExam[]>(
+    [],
+  );
+
+  useEffect(() => {
+    let ignore = false;
+
+    /**
+     * Chiamo l'API per ottenere le iscrizioni dell'utente.
+     */
+    fetch("http://localhost:3000/api/subscriptions")
+      .then((response) => response.json())
+      .then((response: SubscriptionType[]) => {
+        /**
+         * Per ogni iscrizione, chiamo l'API per ottenere i dettagli dell'esame associato.
+         */
+        const mySubscribedExams: Promise<SubscribedExam>[] = response.map(
+          (subscription) => {
+            return fetch(
+              `http://localhost:3000/api/exams/${subscription.exam_id}`,
+            )
+              .then((res) => res.json())
+              .then((exam: ExamType) => ({
+                ...exam,
+                status: subscription.status,
+                grade: subscription.grade,
+              }));
+          },
+        );
+
+        /**
+         * Quando tutte le chiamate sono completate, aggiorno lo stato con l'elenco degli esami.
+         */
+        Promise.all(mySubscribedExams).then((subscribedExams) => {
+          if (!ignore) {
+            setMySubscribedExams(subscribedExams);
+          }
+        });
+      })
+      .catch((error) => {
+        console.error("Errore nel fetch:", error);
+      });
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  return mySubscribedExams;
+};
 
 export const MySubscriptions: React.FC = () => {
+  const mySubscriptions = useMySubscribedExams();
+
   const esamiDaSostenere = mySubscriptions.filter(
-    (subscription) => !subscription.completed,
+    (subscription) => subscription.status === "to-do",
   );
 
   const esamiSostenuti = mySubscriptions.filter(
-    (subscription) => subscription.completed,
+    (subscription) => subscription.status === "completed",
   );
+
+  if (mySubscriptions.length === 0) {
+    return <div>Caricamento in corso...</div>;
+  }
 
   return (
     <>
