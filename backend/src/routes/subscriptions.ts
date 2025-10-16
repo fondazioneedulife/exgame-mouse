eccbe512efd1548cb116ce25ceaa39da55b416cc
Jacopo Sassi <sassijacopp@gmail.com>
2025-10-16 11:38:39 +0200

    feat: imolemented  POST method to create a new subscriptions

--- backend/src/routes/subscriptions.ts
+++ backend/src/routes/subscriptions.ts
@@ -10,4 +10,25 @@ router.get("/", (ctx) => {
   ctx.body = subscriptions;
 });
 
+router.post("/new", (ctx) => {
+
+  try {
+    const newSubscription = { ...ctx.request.body };
+
+    if (!newSubscription || !newSubscription.exam_id || !newSubscription.student_id) {
+      ctx.status = 400;
+      ctx.body = { error: "Parametri mancanti" };
+      return;
+    }
+    subscriptions.push(newSubscription);
+    ctx.status = 201;
+    ctx.body = newSubscription;
+  }
+
+  catch (error) {
+    ctx.status = 500;
+    ctx.body = { error: "Errore durante la creazione della sottoscrizione" };
+  }
+});
+
 export default router;
