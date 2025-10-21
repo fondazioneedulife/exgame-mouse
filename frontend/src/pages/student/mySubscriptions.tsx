f14ab7391b7258a23d0cc14384ce334efa34ea25
littl-bee <isabellacruz1610@gmail.com>
2025-10-21 09:21:23 +0200

    fe lezione 8

--- frontend/src/pages/student/mySubscriptions.tsx
+++ frontend/src/pages/student/mySubscriptions.tsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
 import { Table } from "../../components/Table/Table";
-import { mySubscriptions } from "../../mocks/mySubscriptions";
+import type { ExamType, SubscriptionType } from "../../../../api/types";
 
 export const MySubscriptions: React.FC = () => {
+  // qui vogliamo chiamare l'API per prendere le mie iscrizioni
+  const [mySubscriptions, setMySubscriptions] = useState<SubscriptionType[]>(
+    [],
+  );
+
+  useEffect(() => {
+    // chiamata API per prendere le mie iscrizioni
+    fetch("http://localhost:3000/api/subscriptions")
+      .then((response) => response.json())
+      // .then(setMySubscriptions);
+      .then((mySubscriptions: SubscriptionType[]) => {
+        mySubscriptions.map((mySubscription) => {
+          fetch("http://localhost:3000/api/exams/" + mySubscription.exam_id)
+          .then((response) => response.json())
+          .then((exam: ExamType) => {
+            return {...mySubscription, name: exam.name, date: exam.schedule_date};
+          })
+        })
+      })
+  }, []);
+
   const esamiDaSostenere = mySubscriptions.filter(
-    (subscription) => !subscription.completed,
+    (subscription) => subscription.status === "to-do",
   );
 
   const esamiSostenuti = mySubscriptions.filter(
-    (subscription) => subscription.completed,
+    (subscription) => subscription.status === "completed",
   );
 
   return (
