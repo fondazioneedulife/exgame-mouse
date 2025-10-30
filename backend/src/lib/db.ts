586f9d422a767461d43624a0f7c15c64764bc0bc
Alex Pezzini <alex.pezzini@intesys.it>
2025-10-30 10:29:36 +0100

    Refactor cat model definition to use TypeScript interface for improved type safety; add users model file.

--- backend/src/lib/db.ts
+++ backend/src/lib/db.ts
@@ -3,9 +3,14 @@ import mongoose from "mongoose";
 
 mongoose.connect(config.DB_URL);
 
+interface ICat {
+  name: string;
+  age: number;
+}
+
 const catSchema = new mongoose.Schema({
   name: String,
   age: Number,
 });
 
-const catModel = mongoose.model("Cat", catSchema);
+const catModel = mongoose.model<ICat>("Cat", catSchema);
