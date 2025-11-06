85d9e1554b0d170ecdedde57976115749580eda4
Jacopo Sassi <sassijacopp@gmail.com>
2025-11-06 10:28:43 +0100

    Add CRUD methods to ExamsDao for exam management

--- backend/src/dao/exams.dao.ts
+++ backend/src/dao/exams.dao.ts
@@ -4,6 +4,18 @@ class ExamsDao {
     async getAll() {
         return await examsModel.find({});
     }
+    async getById(id: string) {
+        return await examsModel.findById(id);
+    }
+    async create(exam: any) {
+        return await examsModel.create(exam);
+    }
+    async update(id: string, exam: any) {
+        return await examsModel.findByIdAndUpdate(id, exam, { new: true });
+    }
+    async delete(id: string) {
+        return await examsModel.findByIdAndDelete(id);
+    }
 }
 
 export default ExamsDao;
