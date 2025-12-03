6f0c3531194271eecfe36a54d8d84d47176bedfa
Jacopo Sassi <sassijacopp@gmail.com>
2025-12-03 11:01:26 +0100

    Add Joi and Joy for validation and update dependencies in package.json and package-lock.json

---
+++ backend/src/middlewares/users.ts
@@ -0,0 +1,47 @@
+import { Context, Next } from "koa";
+import Joi from "joi";
+
+export const userSchema = Joi.object({
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
+
+  password: Joi.string()
+    .min(8)
+    .pattern(/[!@#$%^&*(),.?":{}|<>]/)
+    .required()
+});
+
+export const usersValidateMiddleware = async (ctx: Context, next: Next) => {
+   try {
+    const { error } = userSchema.validate(ctx.request.body);
+
+    if (error) {
+      ctx.status = 400;
+      ctx.body = {
+        status: "error",
+        message: error.details[0].message
+      };
+      return;
+    }
+
+    await next();
+
+  } catch (err) {
+    ctx.status = 500;
+    ctx.body = {
+      status: "error",
+      message: "Errore interno del server"
+    };
+  }
+};
+
+export const hashPasswordMiddleware = async (ctx: Context, next: Next) => {
+  const bcrypt = await import("bcryptjs");
+  const saltRounds = 10;
+  const { password } = ctx.request.body;
+
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  ctx.request.body.password = hashedPassword;
+  await next();
+}
